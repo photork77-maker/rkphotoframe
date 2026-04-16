3aac953be2c7f651ba671a3bb756922f7d91c42a
photork77-maker <photork77@gmail.com>
2026-04-16 03:52:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function checkout() {
-alert("Proceeding to payment (Razorpay will be added next)");
+function updatePrice() {
+const frame = parseInt(document.getElementById("frame").value);
+const mount = parseInt(document.getElementById("mount").value);
+const paper = parseInt(document.getElementById("paper").value);
+
+const total = frame + mount + paper;
+document.getElementById("price").innerText = total;
 }
+
+document.getElementById("frame").onchange = updatePrice;
+document.getElementById("mount").onchange = updatePrice;
+document.getElementById("paper").onchange = updatePrice;
+
+updatePrice();
